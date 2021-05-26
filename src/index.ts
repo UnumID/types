0daa38e2a53386747e8045f52343fb85be5fb710
Ray Smets <rayjsmets@gmail.com>
2021-05-26 09:58:49 -0700

    [PresentationRequest] id added for cross version handling.

--- src/index.ts
+++ src/index.ts
@@ -116,6 +116,7 @@ export interface PresentationRequestOptions {
  */
 export interface UnsignedPresentationRequest extends PresentationRequestOptions {
   uuid: string;
+  id?: string; // for related requests across versions
 }
 
 /**
