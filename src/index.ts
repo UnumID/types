728a1f2e2187ca086282021d6d2c1b2d9cf149cd
jacobleesinger <jacobleesinger@gmail.com>
2021-11-02 12:53:18 -0700

    fix EncryptedCredentialsDto

--- src/index.ts
+++ src/index.ts
@@ -142,7 +142,7 @@ export interface EncryptedCredentialDto {
  */
 export interface EncryptedCredentialsDto {
   encryptedCredentials: {
-    [credentialId: string]: EncryptedCredentialDto
+    [credentialId: string]: EncryptedCredentialDto[];
   }
 }
 
