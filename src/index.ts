aeb4a5ec82dbcc0b1c8bc2c5867d0f3198fb4dd5
jacobleesinger <jacobleesinger@gmail.com>
2021-11-04 17:00:57 -0700

    [EncryptedData, EncryptedCredential] update EncryptedData and EncryptedCredential

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,19 @@ import { Literal, Static, Union } from "runtypes";
 import { SemVer } from 'semver';
 import { UnsignedPresentation as UnsignedPresentationPb, Presentation as PresentationPb} from "./protos/presentation";
 import { UnsignedPresentationRequest as UnsignedPresentationRequestPb, PresentationRequest as PresentationRequestPb } from "./protos/presentationRequest"
-import { UnsignedCredential as UnsignedCredentialPb, Credential as CredentialPb, CredentialRequest as CredentialRequestPb, CredentialStatusInfo} from "./protos/credential";
+import {
+  UnsignedCredential as UnsignedCredentialPb,
+  Credential as CredentialPb,
+  CredentialRequest as CredentialRequestPb,
+  CredentialStatusInfo
+} from "./protos/credential";
 import { Proof as ProofPb} from "./protos/proof";
-import { IssueCredentialDto, IssueCredentialsDto, EncryptedCredential} from "./protos/credential"
-import { EncryptedData, EncryptedKey } from "./protos/crypto"
+import {
+  IssueCredentialDto,
+  IssueCredentialsDto,
+  EncryptedCredential as EncryptedCredentialPb
+} from "./protos/credential"
+import { EncryptedData as EncryptedDataPb, EncryptedKey } from "./protos/crypto"
 import { HolderAppInfo } from "./protos/holderApp";
 
 // proto defined types that also have older, vanilla ts types defined - hence the proceeding "Pb"
@@ -24,13 +33,11 @@ export {
   // protos/credential
   IssueCredentialDto,
   IssueCredentialsDto,
-  CredentialStatusInfo,
-  EncryptedCredential
+  CredentialStatusInfo
 }
 
 export {
   // protos/crypto
-  EncryptedData,
   EncryptedKey
 }
 
@@ -136,14 +143,25 @@ export interface Credential extends UnsignedCredential {
 }
 
 /**
+ * Extends protobuf definition to make field required
+ */
+export interface EncryptedData extends EncryptedDataPb {
+  key: EncryptedKey
+}
+
+export interface EncryptedCredential extends EncryptedCredentialPb {
+  encryptedData: EncryptedData;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+/**
  * Data transfer object for a single EncryptedCredential
  * Note: extending the protobuf definition of EncryptedCredential in order to make the date fields string for json serialization
  */
-export interface EncryptedCredentialDto extends EncryptedCredential {
-  uuid: string;
+export interface EncryptedCredentialDto extends Omit<EncryptedCredential, 'createdAt' | 'updatedAt'> {
   createdAt: string; // dates should be converted to ISO strings, since this is how they will be represented in the JSON at runtime
   updatedAt: string; // dates should be converted to ISO strings, since this is how they will be represented in the JSON at runtime
-  version: string;
 }
 
 /**
